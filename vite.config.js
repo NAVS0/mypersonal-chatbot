ca62e20614bf08b497b20fd67639cfddd1d0f305
Navs.Devs <rogelnavarro74@gmail.com>
2025-09-10 19:49:47 +0800

    fix now

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/navs-chatbot",
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || '')
-  },
-  
+
   server: {
     open:true,
     port: 5173,
